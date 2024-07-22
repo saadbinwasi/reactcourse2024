0b1edd436fd3f674e4bdbf5199c62f38b0069832
saad <saad.bin@saad.com>
2024-07-22 19:18:10 +0500

    learned about multiple components in react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   const likes = 10;
   return (
     <div className="App">
+      <Navbar/>
     <div className='content'>
       <h1>APP COMPONENT {name}</h1>
       <p>{likes+1}</p>
