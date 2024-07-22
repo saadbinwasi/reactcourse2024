7b6ddba7b6c702977fde416ac34ef67c3edab568
saad <saad.bin@saad.com>
2024-07-22 19:37:17 +0500

    navbar add with css

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
+    <div className='navbar'>
 
-     <h1>Navbar</h1>   
+        <h1 className='logo'>The Dojo Blog</h1> 
+
+        <div className='links'>
+            <a href='/'>Home</a>
+            <a href='/'>Contacts</a>
+            <a href='/'>Home</a>
+            <a href='/'>About Us</a>
+            <a href='/' style={{color: "green"}}>About Us</a>
+        </div>
+
+  
       
     </div>
   )
