169bf0ef838fe8ef8d7e21d829f2d44d05f0c050
saad <saad.bin@saad.com>
2024-07-22 19:59:14 +0500

    events in react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
 import Navbar from './Navbar';
 
-function App() {
 
-  const name = 'saadbinwasi';
-  const likes = 10;
+function App() {
+  const handleClick = (e) => {
+    console.log('hello',e.target.value)
+  }
+
+
+  const handleClickagain = (name) => {
+    console.log('hello' + name)
+  }
   return (
     <div className="App">
       <Navbar/>
     <div className='content'>
-      <h1>APP COMPONENT {name}</h1>
-      <p>{likes+1}</p>
+
+      <h1>Content</h1>
+
+      <button onClick={handleClick}>click me</button>
+      <button onClick={() => {
+        handleClickagain('saad')}}>click me again</button>
+    
+    
+    
       </div>
     </div>
   );
-}
+} 
 
 export default App;
